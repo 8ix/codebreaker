07953896907a4cbc940c20a3869e99fa97e843e0
Andrew <andrew@8ix.co.uk>
2024-08-06 19:04:52 +0100

    feature: first working version

--- components/GuessHistory/GuessHistory.tsx
+++ components/GuessHistory/GuessHistory.tsx
@@ -1,5 +1,37 @@
 import React, { useContext, useEffect, useState, ReactElement } from 'react';
+import styled from 'styled-components';
 import { GameContext } from '../../context/GameContext';
+
+import StatusLight from '../UI/StatusLight/StatusLight';
+
+const GuessHistoryContainer = styled.div`
+    witdh: 100%;
+`;
+
+const GuessItemBar = styled.ul`
+    display: flex;
+    list-style-type: none;
+    padding: 0;
+`;
+
+const GuessItem = styled.li`
+    display: flex;
+    flex-direction: row;
+    width: 30px;
+    height: 30px;
+    line-height: 30px;
+    margin: 5px;
+    background-color: #fff;
+    color: #000;
+    justify-content: center;
+
+    &.lights {
+        background-color: transparent;
+        padding: 0;
+        width: auto;
+        height: auto;
+    }
+`;
 
 const GuessHistory = () => {
     const { gameEngine, isLoading, gameVersion } = useContext(GameContext);
@@ -11,13 +43,26 @@ const GuessHistory = () => {
             const guessesItems: ReactElement[] = [];
 
             guessHistory.forEach((guess, index) => {
+
+                let guessItems: ReactElement[] = [];
+
+                guess.guess.forEach(element => {
+                    guessItems.push(
+                        <GuessItem key={element}>{element}</GuessItem>
+                    );
+                });
+
                 guessesItems.push(
-                    <div key={index}>
-                        <div>Guess: {guess.guess.join('')}</div>
-                        <div>Perfect Matches: {guess.perfect}</div>
-                        <div>Correct Items: {guess.correct}</div>
-                        <div>Incorrect Items: {guess.incorrect}</div>
-                    </div>
+                    <GuessHistoryContainer key={index}>
+                        <GuessItemBar>
+                            {guessItems}
+                            <GuessItem className="lights">
+                                <StatusLight type="perfect" count={guess.perfect} />
+                                <StatusLight type="correct" count={guess.correct} />
+                                <StatusLight type="incorrect" count={guess.incorrect} />
+                            </GuessItem>
+                        </GuessItemBar>
+                    </GuessHistoryContainer>
                 );
             });
 
